ec797cddf051c192bb19733ab8f24bca579bf708
Daniele Tentoni <d.tentoni@wedoit.io>
2021-11-10 15:21:55 +0100

    Fix multi request.

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.use(express.urlencoded({ extended: false }));
 
 const nodemailer = require("nodemailer");
 
-const send = (msg, res) => {
-  const transport = nodemailer.createTransport({
+const createDefaultProtocol = () =>
+  nodemailer.createTransport({
     service: "gmail",
     host: "smtp.gmail.com",
     auth: {
@@ -19,28 +19,69 @@ const send = (msg, res) => {
       rejectUnauthorized: false,
     },
   });
-  transport.sendMail(
-    {
-      from: `"Try" <${process.env.EMAIL}>`,
-      to: process.env.DEST,
-      subject: "Hello",
-      text: msg,
-    },
-    function (error, result) {
-      if (error) {
-        console.error("Error:", error);
-        return res.status(500).json({ error });
-      }
 
-      console.log("Yes:", result);
-      return res.status(200).json({ result });
-    }
-  );
+/**
+ * Send a text with transport protocol.
+ * @param {*} transport Transport protocol implementation.
+ * @param {*} text Text to send.
+ * @returns Resolved promise.
+ */
+const sendMailWithText = (transport, text) =>
+  new Promise((resolve, reject) => {
+    transport.sendMail(
+      {
+        from: `"Try" <${process.env.EMAIL}>`,
+        to: process.env.DEST,
+        subject: "Hello",
+        text,
+      },
+      function (error, result) {
+        if (error) {
+          console.error("Error:", error);
+          reject(error);
+        }
+
+        console.log("Yes:", result);
+        resolve(result);
+      }
+    );
+  });
+
+const send = (msg) => {
+  const transport = createDefaultProtocol();
+  return sendMailWithText(transport, msg);
 };
 
-app.get("/", (req, res) => send("Hello world!", res));
+app.get("/", (req, res) =>
+  send("Hello world!", res)
+    .then((result) => res.status(200).json({ result }))
+    .catch((error) => res.status(500).json({ error }))
+);
 
-app.get("/text/:msg", (req, res) => send(req.params.msg, res));
+app.get("/text/:msg", (req, res) =>
+  send(req.params.msg, res)
+    .then((result) => res.status(200).json({ result }))
+    .catch((error) => res.status(500).json({ error }))
+);
+
+app.get("/loop/:times/:msg", async (req, res) => {
+  try {
+    const times = Number(req.params.times || 2);
+    console.log("Times:", times);
+    const items = Array.from(Array(times)); //  Array.from(Array(times));
+    console.log("Times:", items);
+    const promises = items.map((each, i) =>
+      send(`${req.params.msg}: ${each}, ${i}`)
+    );
+    console.log("Promises:", promises);
+    const results = await Promise.all(promises);
+    console.log("Results:", results);
+    res.status(200).json({ results });
+  } catch (error) {
+    console.log("Error:", times);
+    res.status(500).json({ error });
+  }
+});
 
 const port = process.env.PORT || 12000;
 app.listen(port, () => {
